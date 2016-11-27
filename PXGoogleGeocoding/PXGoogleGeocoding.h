0c5b766104dc77184e043f7c30b677f8677fb070
romainl <dev.romain@me.com>
2016-11-27 12:19:39 +0100

    Initial Xcode project commit

---
+++ PXGoogleGeocoding/PXGoogleGeocoding.h
@@ -0,0 +1,19 @@
+//
+//  PXGoogleGeocoding.h
+//  PXGoogleGeocoding
+//
+//  Created by Romain on 27/11/2016.
+//  Copyright © 2016 Poulpix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PXGoogleGeocoding.
+FOUNDATION_EXPORT double PXGoogleGeocodingVersionNumber;
+
+//! Project version string for PXGoogleGeocoding.
+FOUNDATION_EXPORT const unsigned char PXGoogleGeocodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PXGoogleGeocoding/PublicHeader.h>
+
+
